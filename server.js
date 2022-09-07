f657735ba2e8a8a0a32feb4c6fcb8a1740d131fb
Phil Neibaur <pneibaur@Phils-MacBook-Pro.local>
2022-09-07 08:00:37 -0600

    adds edit and delete functionality

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const express = require("express");
 const app = express();
 require("dotenv").config();
 const mongoose = require("mongoose");
+const methodOverride = require("method-override");
 // after "mongodb.net/", you can create a new sub-database by writing "booklist", before the "?". found in the .env file...
 const DATABASE_URL = process.env.DATABASE_URL
 const DB = mongoose.connection;
@@ -20,7 +21,7 @@ DB.on("disconnected", ()=> console.log("mongoose disconnected"))
 
 ////////////// MIDDLEWARE
 app.use(express.urlencoded({extended:true}));
-
+app.use(methodOverride("_method"));
 ////////////// ROUTES
 // Seed
 const bookSeed = require("./models/bookSeed.js")
@@ -28,7 +29,7 @@ app.get("/books/seed", (req,res) =>{
     Book.deleteMany({}, (error, allBooks) =>{});
     Book.create(bookSeed, (error, data) =>{
     res.redirect("/books");
-});
+    });
 });
 // Root
 app.get("/", (req,res)=> res.send("<a href='/books'>Link</a> to the books index."));
@@ -46,7 +47,23 @@ app.get("/books/new", (req,res) =>{
     res.render("new.ejs");
 })
 // Destroy
+app.delete("/books/:id", (req, res)=>{
+    Book.findByIdAndDelete(req.params.id, (error, data)=>{
+        res.redirect("/books");
+    });
+});
 // Update
+// server has a route built for PUT requests from the page. 
+app.put("/books/:id", (req, res)=>{
+    if (req.body.completed === "on"){
+        req.body.completed = true;
+    } else {
+        req.body.completed = false;
+    }
+    Book.findByIdAndUpdate(req.params.id, req.body, {new:true}, (error, updatedBook)=>{
+        res.redirect(`/books/${req.params.id}`)
+    });
+})
 // Create
 app.post("/books", (req,res)=> {
     if (req.body.completed === "on") {
@@ -59,6 +76,13 @@ app.post("/books", (req,res)=> {
     });
 });
 // Edit
+app.get("/books/:id/edit", (req, res) =>{
+    Book.findById(req.params.id, (error, foundBook)=>{
+        res.render("edit.ejs", {
+            book: foundBook,
+        })
+    })
+})
 // Show
 app.get("/books/:id", (req, res) =>{
     Book.findById(req.params.id, (error, foundBook) =>{
